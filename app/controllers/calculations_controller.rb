508c8cbedf891fd5b860f75cbc40dc2a9fd07df6
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-07-19 20:34:41 +0000

    Flex_square_root

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,6 +1,20 @@
 class CalculationsController < ApplicationController
     def flex_square
+       # The incoming parameters for this action look like {"a_number" =>"5"}
+       # Rails stores that hash in a variable called params
+       
+       @user_number = params["a_number"].to_i
+       @squared_number = @user_number**2
        
        render("calculations/flexible_square_template.html.erb") 
+    end
+    def flex_square_root
+       # The incoming p:1rameters for this action look like {"a_number" =>"5"}
+       # Rails stores that hash in a variable called params
+       
+       @user_number = params["a_number"].to_i
+       @squareroot_number = @user_number**(0.5)
+       
+       render("calculations/flexible_squareroot_template.html.erb") 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  get("/flexible/square/1", { :controller => "calculations", :action => "flex_square" })
+  get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square" })
+  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root" })
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
