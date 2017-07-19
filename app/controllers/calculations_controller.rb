9428d069eff89e568ce7e473e0e96eba114cc70d
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-07-19 21:07:53 +0000

    flex_payment2

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -17,12 +17,23 @@ class CalculationsController < ApplicationController
        
        render("calculations/flexible_squareroot_template.html.erb") 
     end
-    def monthly_payment
+    def flex_payment
        # The incoming p:1rameters for this action look like {"a_number" =>"5"}
        # Rails stores that hash in a variable called params
        
-       @user_number = params["a_number"].to_i
-       @squareroot_number = @user_number**(0.5)
+       @apr = params["basis_points"].to_i
+       @years = params["number_of_years"].to_i
+       @principal = params["present_value"].to_i
+       
+        rate_as_decimal_permonth = @apr./(100)./(12).to_i
+        number_payments = @years.*(12)
+        one_plus_monthly_rate = 1.+(rate_as_decimal_permonth).to_f
+        discount_factor_numerator = one_plus_monthly_rate.**(number_payments).-(1)
+        discount_factor_denominator1 = (one_plus_monthly_rate).**number_payments
+        discount_factor_denominator = discount_factor_denominator1.*(rate_as_decimal_permonth)
+        discount_factor = discount_factor_numerator./(discount_factor_denominator)
+        
+        @monthly_payment = @principal./(discount_factor)
        
        render("calculations/flexible_monthlypayment_template.html.erb") 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square" })
   get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root" })
-  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "monthly_payment" })
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flex_payment" })
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
