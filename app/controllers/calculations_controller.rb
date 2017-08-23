5dca55b7d268f6f05922e1bfa729ec3fbb945f13
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-08-23 18:53:12 +0000

    flex_payment3

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,7 @@ class CalculationsController < ApplicationController
        # The incoming parameters for this action look like {"a_number" =>"5"}
        # Rails stores that hash in a variable called params
        
-       @user_number = params["a_number"].to_i
+       @user_number = params["a_number"].to_f
        @squared_number = @user_number**2
        
        render("calculations/flexible_square_template.html.erb") 
@@ -12,7 +12,7 @@ class CalculationsController < ApplicationController
        # The incoming p:1rameters for this action look like {"a_number" =>"5"}
        # Rails stores that hash in a variable called params
        
-       @user_number = params["a_number"].to_i
+       @user_number = params["a_number"].to_f
        @squareroot_number = @user_number**(0.5)
        
        render("calculations/flexible_squareroot_template.html.erb") 
@@ -21,17 +21,15 @@ class CalculationsController < ApplicationController
        # The incoming p:1rameters for this action look like {"a_number" =>"5"}
        # Rails stores that hash in a variable called params
        
-       @apr = params["basis_points"].to_i
+       @apr = params["basis_points"].to_i / 100
        @years = params["number_of_years"].to_i
-       @principal = params["present_value"].to_i
+       @principal = params["present_value"].to_f
        
-        rate_as_decimal_permonth = @apr./(100)./(12).to_i
-        number_payments = @years.*(12)
-        one_plus_monthly_rate = 1.+(rate_as_decimal_permonth).to_f
-        discount_factor_numerator = one_plus_monthly_rate.**(number_payments).-(1)
-        discount_factor_denominator1 = (one_plus_monthly_rate).**number_payments
-        discount_factor_denominator = discount_factor_denominator1.*(rate_as_decimal_permonth)
-        discount_factor = discount_factor_numerator./(discount_factor_denominator)
+        number_payments = @years.to_f * 12
+        monthly_interest_rate = (@apr.to_f / 100) / 12
+        discount_factor_numerator = (1+monthly_interest_rate).**(number_payments).-(1)
+        discount_factor_denominator = ((monthly_interest_rate)*(1+monthly_interest_rate)**number_payments)
+        discount_factor = (discount_factor_numerator./(discount_factor_denominator)).to_f
         
         @monthly_payment = @principal./(discount_factor)
        
